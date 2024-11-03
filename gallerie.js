e6b3f1659a7a77385957fe1d8df282766dac7222
klemaire60 <lemaire.kevin6020@gmail.com>
2024-11-03 22:37:26 +0100

    Update

---
+++ gallerie.js
@@ -0,0 +1,33 @@
+// Gallerie.html
+
+function openModal(thumbnail) {
+    var modal = document.getElementById("myModal");
+    var img = modal.querySelector("#img01");
+    //var captionText = modal.querySelector("#caption");
+
+    var imgElement = thumbnail.querySelector("img");
+    
+    img.src = imgElement.src;
+    //captionText.innerHTML = imgElement.alt;
+
+    modal.style.display = "block";
+
+    img.onload = function() {
+        var imgHeight = img.naturalHeight;
+
+        var windowHeight = window.innerHeight;
+        
+        var marginTop = Math.abs((windowHeight - imgHeight) / 2);
+        console.log(imgHeight)
+        console.log(windowHeight)
+        console.log(marginTop)
+
+        // Appliquer la marge calculée à l'image
+        img.style.marginTop = marginTop + "px";
+    };
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none"; 
+}
--- main.js
+++ main.js
@@ -1,31 +1,11 @@
-function openModal(thumbnail) {
-    var modal = document.getElementById("myModal");
-    var img = modal.querySelector("#img01");
-    var captionText = modal.querySelector("#caption");
+document.getElementById('numericArt').addEventListener('click', () => {
+    window.location.href = "gallerie.html?view=numeric";
+});
 
-    var imgElement = thumbnail.querySelector("img");
-    
-    img.src = imgElement.src;
-    captionText.innerHTML = imgElement.alt;
+document.getElementById('draws').addEventListener('click', () => {
+    window.location.href = "gallerie.html?view=draws";
+});
 
-    modal.style.display = "block";
-
-    img.onload = function() {
-        var imgHeight = img.naturalHeight;
-
-        var windowHeight = window.innerHeight;
-        
-        var marginTop = Math.abs((windowHeight - imgHeight) / 2);
-        console.log(imgHeight)
-        console.log(windowHeight)
-        console.log(marginTop)
-
-        // Appliquer la marge calculée à l'image
-        img.style.marginTop = marginTop + "px";
-    };
-}
-
-function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none"; 
-}
+document.getElementById('all').addEventListener('click', () => {
+    window.location.href = "gallerie.html?view=all";
+});
