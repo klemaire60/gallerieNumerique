13c4e3e623c9292d5de218e278868d9e44037e41
klemaire60 <lemaire.kevin6020@gmail.com>
2024-11-04 21:16:52 +0100

    /login

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cors = require("cors");
 
 const config = require("./config.json");
+const connection = require('./db')
 
 const app = express();
 const app80 = express();
@@ -13,6 +14,48 @@ app.use(express.static('public'));
 
 app80.use(cors());
 app80.use(express.static('public'));
+
+function containsInvalidChars(input) {
+    const forbiddenChars = /['";<>]/; // Liste des caractères interdits
+    return forbiddenChars.test(input); // Renvoie true si un des caractères est trouvé
+}
+
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+
+    let errorMessage = '';
+    
+    if(!username) errorMessage += "Le nom d'utilisateur est manquant pour la connexion. ";
+    if(!password) errorMessage += "Le mot de passe est manquant pour la connexion. ";
+    if(username && containsInvalidChars(username)) errorMessage += "Le nom d'utilisateur contient des caractères interdit. ";
+    if(password && containsInvalidChars(password)) errorMessage += "Le mot de passe contient des caractères interdits. ";
+
+    if (errorMessage !== '') {
+        return res.status(400).json({ message: errorMessage });
+    }
+
+    const sqlLogin = 'SELECT password FROM user WHERE usernmae = ?';
+
+    connection.query(sqlLogin, [username], (err, results) => {
+        if(err) {
+            console.error("Erreur SQL lors de la requête sqlLogin\n erreur :\n", err);
+            return res.status(500).json({message : "Erreur lors de la connexion"});
+        }
+
+        if(results.lenght == 0) 
+        {
+            return res.status(401).json({ message: "Aucun compte trouvé avec ce nom d'utilisateur"});
+        }
+        
+        let token = jwt.sign({ mail }, config.jwtKey);
+        res.cookie('userToken', token, {
+            httpOnly: true,
+            secure: false,
+            maxAge: 43200000
+        })
+        return res.status(200).json({ message: "Connexion Réussie"});
+    })
+})
 
 app.get('/images', async (req, res) => {
     const view = req.query.view;
